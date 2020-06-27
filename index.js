d45aeb614f4daff9894aadb03a805ada18554109
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-06-27 11:29:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
   if(event.message.text === "archo"){
+    client.pushMessage("U3c62a1ade09ec47084828d746e778c15","I'm Archo.");
+    console.log("Message pushed");
     return client.replyMessage(event.replyToken, { type: 'text', text: "Hi!" });
-    client.pushMessage("U3c62a1ade09ec47084828d746e778c15","I'm Archo.")
-    console.log("Message pushed")
   }else{
 
   // create a echoing text message
