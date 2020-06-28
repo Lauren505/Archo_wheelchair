5b6f6891353722efd557a17560d3c4ee7196e71f
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-06-28 15:49:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -68,15 +68,15 @@ function handleEvent(event) {
   }
 }
 
-app.get('/send',(req,res) =>{
+app.get('/send/:name',(req,res) =>{
     let message = {
         type: 'text',
-        text: 'Hello World!'
+        text: req.params.name
       };
     client.pushMessage("U3c62a1ade09ec47084828d746e778c15",message)
     .then(() => {
         console.log("Message pushed")
-        res.send("message sent")
+        res.send("message sent" + req.params.name)
     })
     .catch((err) => {
         res.send("error pushing message")
