8b3f3f7fe456298021bf535ed84f210a998ba2fc
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-06-28 17:44:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,11 @@ app.get('/test',(req,res) =>{
     // error handling
     });
 })
-
+app.post('/sth', function(req, res) {
+    console.log(req.query.id);
+    console.log(req.body.name);
+    console.log(req.body.tel);
+});
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
