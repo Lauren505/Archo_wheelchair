4d16d7e3487dc1f4dd5019c227f244c8038b936f
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-06-29 11:22:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -81,19 +81,14 @@ app.use(bodyParser.json());
 app.post('/test',function(req,res){   // JSON資料的處理程式
   var json=req.body;   // 取出POST資料本體
   console.log(json);
-  let message = {
-        type: 'text',
-        text: JSON.stringify(req.body.Direction)
-      };
-    client.pushMessage("U3c62a1ade09ec47084828d746e778c15",message)
-    .then(() => {
-        console.log("Message pushed")
-        res.send("msg from post")
-    })
-    .catch((err) => {
-        res.send("error pushing message")
-    // error handling
-    });
+  .then(() => {
+      console.log("Message pushed")
+      res.send("msg from post")
+  })
+  .catch((err) => {
+      res.send("error pushing message")
+  // error handling
+  });
   yourdata = req.body.Direction;
 });
 
